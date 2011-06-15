db60b0027fb3a7e1da477d92769374a5c1e332fe
Rob Hoelz <rob@hoelz.ro>
2011-06-14 22:17:33 -0500

    Initial commit

---
+++ lib/Plack/Test/AnyEvent.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Plack::Test::AnyEvent;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Plack::Test::AnyEvent
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
