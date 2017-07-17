8f7e791e00ef322b9465caf6c4c5e86e7785cfc6
Rob Hoelz <rob@hoelz.ro>
2017-07-17 14:48:27 -0500

    [appveyor] debugging

--- lib/Plack/Test/AnyEvent.pm
+++ lib/Plack/Test/AnyEvent.pm
@@ -49,7 +49,7 @@ sub test_psgi {
 
                 unless(defined $body) {
                     ( $read, $write ) = winsocketpair();
-                    $write = IO::Handle->new_from_fd($write, 'w');
+                    $write = IO::Handle->new_from_fd($write, 'w') or die "shit: $!";
                     $write->autoflush(1);
                     return $write;
                 }
